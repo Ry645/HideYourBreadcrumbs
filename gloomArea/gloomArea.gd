b93bffd3bd6c90da30ed4ee5cbadd1ff405b03f0
Ryan Sexton <ry645.dev@gmail.com>
2024-04-06 15:08:52 -0400

    nope I'm using fog instead I will change the exposure on the player camera to pitch black when they enter the gloom Area

--- gloomArea/gloomArea.gd
+++ gloomArea/gloomArea.gd
@@ -1,11 +1,14 @@
 extends Area3D
 
+@onready var collision_shape_3d:CollisionShape3D = %CollisionShape3D
+@onready var fog_volume = %FogVolume
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#needed to get global position
 	#print($"../room3/dresser2/gloomBoxTransform".global_position)
-	pass
+	fog_volume.size = collision_shape_3d.shape.size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
