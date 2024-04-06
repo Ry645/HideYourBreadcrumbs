a30522009189991cce684826f5d33ce41250c8c0
Ryan Sexton <ry645.dev@gmail.com>
2024-04-06 00:09:39 -0400

    using simple black mesh with inside and outside faces for gloom effect no volumetric fog since it's too finicky

---
+++ gloomArea/gloomArea.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#needed to get global position
+	#print($"../room3/dresser2/gloomBoxTransform".global_position)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
